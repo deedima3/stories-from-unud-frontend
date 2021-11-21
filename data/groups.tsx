a19654bcca773ce2156dd5968d31382919c8afd1
Bianca Saraswati <biancasaraswati@gmail.com>
2021-11-21 12:47:48 +0800

    groupName

--- data/groups.tsx
+++ data/groups.tsx
@@ -16,12 +16,12 @@ export const Anggota = [
         "github" : "https://github.com/deedima3?tab=repositories"
     },
     {
-        "img" : "/dima.png",
-        "nama" : "Gede Sudimahendra",
-        "nim" : "2008561057",
-        "desc" : "Bocah penyuka kocheng yang wibu",
-        "ig" : "https://www.instagram.com/deedima3/?hl=en",
-        "github" : "https://github.com/deedima3?tab=repositories"
+        "img" : "/bianca.png",
+        "nama" : "I Gusti Agung Istri Bianca Githa Saraswati",
+        "nim" : "2008561087",
+        "desc" : "Computer science girl yang noob ngoding",
+        "ig" : "https://www.instagram.com/biancasaraswatii",
+        "github" : "https://github.com/biancagsp"
     },
     {
         "img" : "/dima.png",
