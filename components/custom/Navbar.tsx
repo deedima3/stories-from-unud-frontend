d815a55e8412072c3427d73569b25b4fab879f82
Gede Sudimahendra <gedesudimahendra@gmail.com>
2021-11-29 11:58:13 +0800

    Minor changes in Navbar and login

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -47,7 +47,15 @@ const NavbarDesktop = () => {
 const NavbarMobile = () => {
     const [isOpened, setOpened] = useState(false)
     return(
-
+        <div className="flex flex-row items-center h-10 w-max justify-between flex-shrink-0">
+            <NavbarLink text="Home" path="/"/>
+            <NavbarLink text="Blog Post" path="/blog"/>
+            <NavbarLink text="Our Group" path="/our"/>
+            <NavbarLink text="New Post" path="/new"/>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10">
+                <Link href="/login">Login</Link>
+            </button>
+        </div>
     )
 }
 
