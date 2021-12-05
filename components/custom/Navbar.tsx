6874962094759d00b18db1855c9b2d630b468271
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2021-12-06 01:17:48 +0800

    Add Admin Navbar and Admin Context

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Link from "next/link"
 import router, { useRouter } from 'next/router'
 import NavbarLink from './NavbarLink';
+import { UserContext } from '../../pages/_app';
+import Router from 'next/router'
 
 const Navbar = () => {
     const [isNavbarStick, setIsNavbarStick] = useState(false);
@@ -18,6 +20,7 @@ const Navbar = () => {
       useEffect(() => {
           
       })
+      
     return (
         <nav className={`bg-white w-screen flex-row flex justify-center sticky top-0 z-50 ${isNavbarStick ? "shadow-md" : ""}`}>
             <div className="flex justify-between flex-row w-4/5 h-20 items-center">
@@ -30,31 +33,72 @@ const Navbar = () => {
     )
 }
 
+const AdminNav = () => {
+    const {user, setUser} = useContext(UserContext)
+
+    const handleLogout = () => {
+        fetch('api/logout', {
+            method : "POST",
+            headers : {
+                'sessionID' : user
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if(data){
+                setUser(user => '')
+                Router.push("/")
+            }
+        })
+    }
+
+    return (
+        <>
+            <NavbarLink text="Validator" path="/validator"/>
+            <Link href="/logout">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10" onClick={handleLogout}>
+                    Logout
+                </button>
+            </Link>
+        </>
+    )
+}
+
+const UserNav = () => {
+    return(
+        <Link href="/login">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10">
+                Login
+            </button>
+        </Link>
+    )
+}
+
 const NavbarDesktop = () => {
+    const {user, setUser} = useContext(UserContext)
+
     return(
         <div className="flex flex-row items-center h-10 w-max justify-between flex-shrink-0">
             <NavbarLink text="Home" path="/"/>
             <NavbarLink text="Blog Post" path="/blog"/>
             <NavbarLink text="Our Group" path="/our"/>
             <NavbarLink text="New Post" path="/new"/>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10">
-                <Link href="/login">Login</Link>
-            </button>
+            {user ? <AdminNav/> : <UserNav/> }
         </div>
     )
 }
 
 const NavbarMobile = () => {
     const [isOpened, setOpened] = useState(false)
+    const {user, setUser} = useContext(UserContext)
+
     return(
         <div className="flex flex-row items-center h-10 w-max justify-between flex-shrink-0">
             <NavbarLink text="Home" path="/"/>
             <NavbarLink text="Blog Post" path="/blog"/>
             <NavbarLink text="Our Group" path="/our"/>
             <NavbarLink text="New Post" path="/new"/>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10">
-                <Link href="/login">Login</Link>
-            </button>
+            {user ? <AdminNav/> : <UserNav/> }
         </div>
     )
 }
